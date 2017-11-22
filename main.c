644919d224091865cc18b0d31c1025018fb7b964
Mykhailo <0660330233@ukr.net>
2017-11-22 23:16:28 +0200

    Checking

--- main.c
+++ main.c
@@ -11,24 +11,39 @@
 /* ************************************************************************** */
 
 #include "funclib.h"
+#include <stdio.h>
 
-int	main(int arg, char **argv)
+char *read_list(char **argv, char *buffer)
 {
 	int		fd;
 	size_t	block;
-	char buffer [21 * 26];
-
-	block = sizeof(char) * 21;
+	size_t	len;
 	
+	len = 550;
+	if ((fd = open(argv[1], O_RDONLY)) == -1)
+		return (0);
+	block = read(fd, buffer, len);
+	if (block < 20 || block > 545)
+	{
+		ft_putstr("error\n");
+		exit(1);	
+	}
+	close(fd);
+	return (buffer);
+}
+
+int	main(int arg, char **argv)
+{	
+	char buffer [550];
+	char	*list_buf;
+
+	//Заполнить буфер символами конца строки для эстетики. 
 	if (arg != 2)
 	{
 		ft_putstr("Usage: You must take only one parametr!\n");
 		exit(1);
 	}
-	if ((fd = open(argv[1], O_RDONLY)) == -1)
-		return (0);
-	while ((block = read(fd, buffer, (sizeof(char) * 21)) > 0)
-		   
-	close(fd);
+	list_buf = read_list(argv, buffer);
+	printf("%s\n", list_buf + 525);
 	return (0);
 }
